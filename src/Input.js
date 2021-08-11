434b13e50e095110478336884f209570742de660
ZumTheZazaKing <zahidi85543@gmail.com>
2021-08-11 18:43:44 +0800

    Minor Changes

--- src/Input.js
+++ src/Input.js
@@ -8,7 +8,7 @@ export function Input(props){
             <input type="Text" placeholder="No more than 20 words" value={props.inputTitle} maxLength={20} onChange={props.changeTitle} required/>
             <br/><br/>
             <label>Amount:<br/>e.g. 50 = Income<br/>e.g. -100 = Expense</label><br/>
-            <input type="number" value={props.inputAmount} onChange={props.changeAmount} required/>
+            <input type="number" placeholder="e.g. 50, -100" value={props.inputAmount} onChange={props.changeAmount} required/>
             <br/><br/>
             <input type="submit" value="Add Transaction"/>
         </form>
